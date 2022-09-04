4dd3152f2824072b85a688ba99d616fedbe41fa6
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-09-05 00:51:36 +0200

    typography reset

---
+++ curiosity/src/main/java/com/hello/curiosity/compose/ui/components/text/Label.kt
@@ -0,0 +1,20 @@
+package com.hello.curiosity.compose.ui.components.text
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Label(
+    text: String,
+) = Text(
+    text = text,
+    style = MaterialTheme.typography.h3
+)
+
+@Preview
+@Composable
+fun LabelPreview() {
+    Label(text = "Label")
+}
