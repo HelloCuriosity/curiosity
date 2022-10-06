9f886f9b07b6ec0015c89eccd471f5c16605b029
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-06 22:00:07 +0200

    lint

--- app/src/main/java/com/hello/curiosity/curiosity/ui/scenes/DashboardScene.kt
+++ app/src/main/java/com/hello/curiosity/curiosity/ui/scenes/DashboardScene.kt
@@ -5,14 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hello.curiosity.compose.ui.components.BottomNavigation
 import com.hello.curiosity.curiosity.navigation.DashboardNavigationConfiguration
-import com.hello.curiosity.curiosity.ui.scenes.color.ColorScene
-import com.hello.curiosity.curiosity.ui.scenes.type.TypographyScene
 
 @Composable
 fun DashboardScene() {
