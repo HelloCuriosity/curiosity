ab24102880c45760dc2fa3cd154348bead678820
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-06 21:40:47 +0200

    [Housekeeping] Navigation

--- app/src/main/java/com/hello/curiosity/curiosity/ui/scenes/DashboardScene.kt
+++ app/src/main/java/com/hello/curiosity/curiosity/ui/scenes/DashboardScene.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hello.curiosity.compose.ui.components.BottomNavigation
+import com.hello.curiosity.curiosity.navigation.DashboardNavigationConfiguration
 import com.hello.curiosity.curiosity.ui.scenes.color.ColorScene
 import com.hello.curiosity.curiosity.ui.scenes.type.TypographyScene
 
@@ -35,16 +36,4 @@ fun DashboardScene() {
     }
 }
 
-@Composable
-private fun DashboardNavigationConfiguration(
-    navController: NavHostController
-) {
-    NavHost(navController, startDestination = BottomNavScenes.Color.route) {
-        composable(BottomNavScenes.Color.route) {
-            ColorScene()
-        }
-        composable(BottomNavScenes.Typography.route) {
-            TypographyScene()
-        }
-    }
-}
+
