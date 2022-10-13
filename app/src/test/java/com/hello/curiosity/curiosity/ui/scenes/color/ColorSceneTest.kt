02a76d50af1dacd27c034f22afb2831b95ed2e6d
Kyle Roe <hopeman15@users.noreply.github.com>
2022-10-13 23:03:11 +0200

    [Testing] Color Scene (#29)
    * WIP
    
    * color scene test
    
    * ColorViewTest
    
    Co-authored-by: Kyle Roe <[hopeman15@users.noreply.github.com]>

---
+++ app/src/test/java/com/hello/curiosity/curiosity/ui/scenes/color/ColorSceneTest.kt
@@ -0,0 +1,23 @@
+package com.hello.curiosity.curiosity.ui.scenes.color
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import com.hello.curiosity.test.compose.ComposeTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ColorSceneTest : ComposeTest() {
+
+    @Test
+    fun testColorScene() {
+        composeTestRule.setContent {
+            ColorScene()
+        }
+
+        composeTestRule
+            .onNodeWithTag("lazy-colors-tag")
+            .assertIsDisplayed()
+    }
+}
