08efae1cd68a131f39913879aedfca63869bcb0e
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-19 23:29:55 +0200

    [Testing] TypographyScene

---
+++ app/src/test/java/com/hello/curiosity/curiosity/ui/scenes/type/TypographySceneKtTest.kt
@@ -0,0 +1,22 @@
+package com.hello.curiosity.curiosity.ui.scenes.type
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import com.hello.curiosity.test.compose.ComposeTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class TypographySceneKtTest : ComposeTest() {
+    @Test
+    fun testColorScene() {
+        composeTestRule.setContent {
+            TypographyScene()
+        }
+
+        composeTestRule
+            .onNodeWithTag("lazy-types-tag")
+            .assertIsDisplayed()
+    }
+}
