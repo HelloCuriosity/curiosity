642cae86e7140a43ed3196c73487554edd525bfb
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-20 08:42:30 +0200

    [Housekeeping] Remove MainActivity Compose Preview

--- app/src/main/java/com/hello/curiosity/curiosity/MainActivity.kt
+++ app/src/main/java/com/hello/curiosity/curiosity/MainActivity.kt
@@ -2,9 +2,6 @@ package com.hello.curiosity.curiosity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.hello.curiosity.compose.ui.theme.CuriosityTheme
 import com.hello.curiosity.curiosity.ui.scenes.DashboardScene
 import com.hello.curiosity.curiosity.ui.theme.ComposeContentView
 
@@ -14,13 +11,5 @@ class MainActivity : ComponentActivity() {
         ComposeContentView {
             DashboardScene()
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CuriosityTheme {
-        DashboardScene()
     }
 }
