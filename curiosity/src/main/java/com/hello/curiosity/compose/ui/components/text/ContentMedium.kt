83a1292e27146ddd4828f84176fb18c8a67943ea
Kyle Roe <hopeman15@users.noreply.github.com>
2022-10-24 14:15:05 +0200

    [Enhancement] Text Component Configurations (#46)
    Co-authored-by: Kyle Roe <[hopeman15@users.noreply.github.com]>

--- curiosity/src/main/java/com/hello/curiosity/compose/ui/components/text/ContentMedium.kt
+++ curiosity/src/main/java/com/hello/curiosity/compose/ui/components/text/ContentMedium.kt
@@ -1,9 +1,13 @@
 package com.hello.curiosity.compose.ui.components.text
 
+import androidx.annotation.StringRes
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -11,12 +15,30 @@ import androidx.compose.ui.tooling.preview.Preview
 fun ContentMedium(
     text: String,
     modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
     textAlign: TextAlign? = null,
+    style: TextStyle = MaterialTheme.typography.body2,
 ) = Text(
     text = text,
     modifier = modifier,
+    color = color,
     textAlign = textAlign,
-    style = MaterialTheme.typography.body2
+    style = style
+)
+
+@Composable
+fun ContentMedium(
+    @StringRes text: Int,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    style: TextStyle = MaterialTheme.typography.body2
+) = Text(
+    text = stringResource(id = text),
+    modifier = modifier,
+    color = color,
+    textAlign = textAlign,
+    style = style
 )
 
 @Preview
--- curiosity/src/test/java/com/hello/curiosity/compose/ui/components/text/ContentLargeTest.kt
+++ curiosity/src/test/java/com/hello/curiosity/compose/ui/components/text/ContentLargeTest.kt
@@ -1,19 +1,32 @@
 package com.hello.curiosity.compose.ui.components.text
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.hello.curiosity.compose.ui.ComposeTextTest
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class ContentLargeTest : ComposeTextTest() {
-    override val text: String = "Label Text"
-    override val content: @Composable () -> Unit = {
+    override val contentString: @Composable () -> Unit = {
         ContentLarge(
             text = text,
             modifier = Modifier,
+            color = Color.White,
             textAlign = null,
+            style = MaterialTheme.typography.h1,
+        )
+    }
+
+    override val contentResource: @Composable () -> Unit = {
+        ContentLarge(
+            text = res,
+            modifier = Modifier,
+            color = Color.White,
+            textAlign = null,
+            style = MaterialTheme.typography.h1,
         )
     }
 }
