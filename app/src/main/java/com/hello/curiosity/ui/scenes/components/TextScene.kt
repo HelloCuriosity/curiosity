4848d9ce63b2cfb609197a66fb52c717aabc6595
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-26 19:17:33 +0200

    [Housekeeping] Remove Redundant Scene Directory

--- app/src/main/java/com/hello/curiosity/ui/scenes/components/TextScene.kt
+++ app/src/main/java/com/hello/curiosity/ui/scenes/components/TextScene.kt
@@ -1,4 +1,4 @@
-package com.hello.curiosity.ui.scenes.components.text
+package com.hello.curiosity.ui.scenes.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
