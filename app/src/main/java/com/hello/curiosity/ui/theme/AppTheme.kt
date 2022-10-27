b5f075c213d5a25190d893b1b74da3674c9bb89c
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-27 18:56:11 +0200

    type

--- app/src/main/java/com/hello/curiosity/ui/theme/AppTheme.kt
+++ app/src/main/java/com/hello/curiosity/ui/theme/AppTheme.kt
@@ -2,13 +2,25 @@ package com.hello.curiosity.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.hello.curiosity.R
 import com.hello.curiosity.compose.ui.components.input.inputTextFieldColors
 import com.hello.curiosity.compose.ui.theme.ColorScheme
 import com.hello.curiosity.compose.ui.theme.Theme
 import com.hello.curiosity.compose.ui.theme.Type
 
 object AppTheme : Theme {
-    override val typography: Type = Type()
+    override val typography: Type = Type(
+        defaultFontFamily = FontFamily(
+            Font(R.font.rubik_light, FontWeight.Light),
+            Font(R.font.rubik_regular, FontWeight.Normal),
+            Font(R.font.rubik_medium, FontWeight.Medium),
+            Font(R.font.rubik_semi_bold, FontWeight.SemiBold),
+            Font(R.font.rubik_extra_bold, FontWeight.ExtraBold),
+        )
+    )
     override val colors: ColorScheme = ColorScheme(
         background = Color(0xFFD0F4EA),
         primary = Color(0xFF087E8B),
