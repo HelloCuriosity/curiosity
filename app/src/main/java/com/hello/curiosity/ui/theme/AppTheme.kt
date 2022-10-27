7bb6358df6ad28417e196e04beacf7054b2c7fd7
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-27 18:04:56 +0200

    dark mode

--- app/src/main/java/com/hello/curiosity/ui/theme/AppTheme.kt
+++ app/src/main/java/com/hello/curiosity/ui/theme/AppTheme.kt
@@ -9,7 +9,18 @@ import com.hello.curiosity.compose.ui.theme.Type
 
 object AppTheme : Theme {
     override val typography: Type = Type()
-    override val colors: ColorScheme = ColorScheme()
+    override val colors: ColorScheme = ColorScheme(
+        background = Color(0xFFD0F4EA),
+        primary = Color(0xFFF9E20A),
+
+        backgroundDark = Color(0xFF3C3C3C),
+        primaryDark = Color(0xFFD0F4EA),
+        surfaceDark = Color(0xFFD0F4EA),
+        onSurfaceDark = Color(0xFF087E8B),
+        primaryVariantDark = Color(0xFFFF5A5F),
+    )
+
+    val topAppBar: Color = Color.Black
 }
 
 @Composable
