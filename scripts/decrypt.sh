8984bc1ee8d17fbec1a08ac422e9db5546c70ab1
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-28 21:11:08 +0200

    scripts

---
+++ scripts/decrypt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+IN=$1
+OUT=$2
+PASSWORD=$3
+
+# Start decrypting
+echo "Decrypting ${IN} to ${OUT} ${PASSWORD}"
+
+gpg --batch --yes --passphrase ${PASSWORD} -o ${OUT} -d ${IN}
