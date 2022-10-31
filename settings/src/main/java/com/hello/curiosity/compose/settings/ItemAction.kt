0122256f69f1322319c6ee7a12e10ace3a60f559
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-10-31 22:57:11 +0100

    item action and test

---
+++ settings/src/main/java/com/hello/curiosity/compose/settings/ItemAction.kt
@@ -0,0 +1,63 @@
+package com.hello.curiosity.compose.settings
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hello.curiosity.compose.ui.components.text.LabelMedium
+
+@Composable
+fun ItemAction(
+    @StringRes title: Int,
+    @DrawableRes icon: Int,
+    @StringRes contentDescription: Int,
+    textColor: Color = Color.Unspecified,
+    tint: Color = Color.Unspecified,
+    onClick: () -> Unit,
+) = Row(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(24.dp)
+        .clickable(onClick = onClick)
+        .testTag("icon-action-test-tag"),
+    horizontalArrangement = Arrangement.SpaceBetween,
+    verticalAlignment = Alignment.CenterVertically,
+) {
+    LabelMedium(
+        text = title,
+        color = textColor,
+    )
+    Icon(
+        modifier = Modifier
+            .padding(start = 24.dp)
+            .testTag("icon-test-tag"),
+        painter = painterResource(id = icon),
+        contentDescription = stringResource(id = contentDescription),
+        tint = tint,
+    )
+}
+
+@Composable
+@Preview
+fun ItemActionPreview() {
+    ItemAction(
+        title = android.R.string.copy,
+        icon = android.R.drawable.ic_delete,
+        contentDescription = android.R.string.copy,
+        tint = Color.Unspecified
+    ) {
+    }
+}
