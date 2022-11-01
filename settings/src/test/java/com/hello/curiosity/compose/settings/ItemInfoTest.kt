5a25d4b8ae8f0be4b0150d0ac96d77d3877ec411
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-11-01 14:52:40 +0100

    item info and test

---
+++ settings/src/test/java/com/hello/curiosity/compose/settings/ItemInfoTest.kt
@@ -0,0 +1,77 @@
+package com.hello.curiosity.compose.settings
+
+import android.content.Context
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.core.app.ApplicationProvider
+import com.hello.curiosity.test.compose.ComposeTest
+import org.junit.Test
+
+class ItemInfoTest : ComposeTest() {
+    private val ctx: Context = ApplicationProvider.getApplicationContext()
+
+    private val titleRes = android.R.string.copy
+    private val title = ctx.getString(titleRes)
+    private val contentRes = android.R.string.fingerprint_icon_content_description
+    private val content = ctx.getString(contentRes)
+
+    @Test
+    fun `validate item info defaults`() {
+        composeTestRule.setContent {
+            ItemInfo(
+                title = titleRes,
+                content = contentRes,
+            )
+        }
+
+        // validate container
+        composeTestRule
+            .onNodeWithTag(testTag = "item-info-test-tag")
+            .assertExists()
+            .assertIsDisplayed()
+
+        // title exists
+        composeTestRule
+            .onNodeWithText(title)
+            .assertExists()
+            .assertIsDisplayed()
+
+        // content exists
+        composeTestRule
+            .onNodeWithText(content)
+            .assertExists()
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `validate item info customization`() {
+        composeTestRule.setContent {
+            ItemInfo(
+                title = titleRes,
+                content = contentRes,
+                titleColor = Color.Blue,
+                contentColor = Color.Blue,
+            )
+        }
+
+        // validate container
+        composeTestRule
+            .onNodeWithTag(testTag = "item-info-test-tag")
+            .assertExists()
+            .assertIsDisplayed()
+
+        // title exists
+        composeTestRule
+            .onNodeWithText(title)
+            .assertExists()
+            .assertIsDisplayed()
+
+        // content exists
+        composeTestRule
+            .onNodeWithText(content)
+            .assertExists()
+            .assertIsDisplayed()
+    }
+}
