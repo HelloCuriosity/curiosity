84b68dca9ec82434938ce468c26652e966b573c8
Kyle Roe <hopeman15@users.noreply.github.com>
2022-11-20 22:21:43 +0100

    [Enhancement] NavigationTest Helper (#70)
    * [Enhancement] NavigationTest Helper
    
    * readme
    
    Co-authored-by: Kyle Roe <[hopeman15@users.noreply.github.com]>

---
+++ test-compose-utils/src/main/java/com/hello/curiosity/test/compose/NavigationTest.kt
@@ -0,0 +1,30 @@
+package com.hello.curiosity.test.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Assert.assertEquals
+import org.junit.Before
+
+abstract class NavigationTest : ComposeTest() {
+
+    protected lateinit var navController: TestNavHostController
+
+    protected abstract val navHost: @Composable () -> Unit
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            navHost.invoke()
+        }
+    }
+
+    fun assertNavigateToDestination(scene: String) {
+        navController.navigate(scene)
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, scene)
+    }
+}
