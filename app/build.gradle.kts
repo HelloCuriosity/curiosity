f25e60954f405d6f2e6a670f4537b06e8d2e48fd
Kyle Roe <hopeman15@users.noreply.github.com>
2023-04-19 14:36:32 +0200

    [Housekeeping] Java 17 Support (#140)
    Co-authored-by: Kyle Roe <[hopeman15@users.noreply.github.com]>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,12 +60,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -76,7 +76,7 @@ android {
         kotlinCompilerExtensionVersion = "1.4.5"
     }
 
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
