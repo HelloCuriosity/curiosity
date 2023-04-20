6b8749e66ef52c3162b1b5f2060b0255287f3c7b
Kyle Roe <[hopeman15@users.noreply.github.com]>
2023-04-20 11:25:27 +0200

    publishing

--- slack-feedback/build.gradle.kts
+++ slack-feedback/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
     kotlin("plugin.serialization") version "1.8.20"
+
+    // Publishing
+    id("maven-publish")
+    signing
 }
 
 java {
@@ -31,4 +35,65 @@ dependencies {
     // Ktor
     testImplementation("io.ktor:ktor-client-mock:2.2.4")
     testImplementation("org.slf4j:slf4j-simple:2.0.7")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { System.getenv("CI") == "true" }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+            groupId = "io.github.hellocuriosity"
+            artifactId = "slack-feedback"
+
+            pom {
+                name.set("Slack Feedback")
+                description.set("Slack Feedback is an easy way to collect feedback, and have it posted to a slack channel.")
+                url.set("https://github.com/HelloCuriosity/curiosity")
+                licenses {
+                    license {
+                        name.set("MIT Licence")
+                        url.set("https://github.com/HelloCuriosity/curiosity/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hopeman15")
+                        name.set("Kyle Roe")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/HelloCuriosity/curiosity.git")
+                    developerConnection.set("scm:git:https://github.com/HelloCuriosity/curiosity.git")
+                    url.set("https://github.com/HelloCuriosity/curiosity")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "MavenCentral"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+
+            credentials {
+                username = System.getenv("SONATYPE_USER")
+                password = System.getenv("SONATYPE_PWD")
+            }
+        }
+    }
+
+    signing {
+        val signingKey: String? = System.getenv("SIGNING_KEY")
+        val signingPwd: String? = System.getenv("SIGNING_PWD")
+        useInMemoryPgpKeys(signingKey, signingPwd)
+        sign(publishing.publications["release"])
+    }
 }
