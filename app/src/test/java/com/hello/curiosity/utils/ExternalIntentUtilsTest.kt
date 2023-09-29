16154a1e1f675c8f86addbff5371ce17a905c591
Kyle <[hopeman15@users.noreply.github.com]>
2023-09-29 09:53:56 +0200

    config robo to SDK 33

--- app/src/test/java/com/hello/curiosity/utils/ExternalIntentUtilsTest.kt
+++ app/src/test/java/com/hello/curiosity/utils/ExternalIntentUtilsTest.kt
@@ -8,8 +8,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [33])
 class ExternalIntentUtilsTest {
 
     @Test
