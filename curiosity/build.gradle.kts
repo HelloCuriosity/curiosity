c94ecbfba4ae78b12ab0a40195d7aacb4cabc253
Kyle Roe <[hopeman15@users.noreply.github.com]>
2024-11-01 12:48:00 +0100

    update compose compiler configs

--- curiosity/build.gradle.kts
+++ curiosity/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 
     // Publishing
     id("maven-publish")
@@ -37,10 +38,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
     }
 
     publishing {
