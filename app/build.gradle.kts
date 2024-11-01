dd70ac6d142f3c7d7153cb3e93e3fba67a31b033
Kyle Roe <[hopeman15@users.noreply.github.com]>
2024-11-01 13:39:08 +0100

    fix dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     // Compose
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("androidx.navigation:navigation-compose:2.8.3")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
